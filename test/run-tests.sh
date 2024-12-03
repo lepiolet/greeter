4a3da5d2cbfc07a933e5553254bdcd316d53dcb7
Aitor Iturri <aitor.iturri@appgate.com>
2024-12-03 18:35:13 +0100

    HArcode the path

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -4,8 +4,4 @@ set -xe
 
 echo "* Running tests"
 echo "  + Test 1"
-mkdir -p greeter
-if [ ! -x greeter/greeter ]; then
-    cp build/greeter greeter/greeter
-fi
 test "$(./greeter/greeter --no-style --name Aitor)" = 'Kaixo Aitor!'
