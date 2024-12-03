088a7f1b042c36d95d25cc40a085cdbd994019d6
Aitor Iturri <aitor.iturri@appgate.com>
2024-12-03 17:56:58 +0100

    Use Makefile instead of Ninja

--- tool/build.sh
+++ tool/build.sh
@@ -2,12 +2,10 @@
 
 set -ex
 
-apt-get update
-apt-get install -y ninja-build
 git submodule init
 git submodule update
 rm -r build && mkdir build
 cd build
-cmake .. -GNinja
+cmake .. -G'Unix Makefiles'
 cmake --build . --target all
 cd ..
