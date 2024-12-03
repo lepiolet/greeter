07b7983c4d9d0663c3a8e4e42b0b967194bb8845
Aitor Iturri <aitor.iturri@appgate.com>
2024-12-03 18:18:32 +0100

    Make greeter path dynamic

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -2,4 +2,9 @@
 
 echo "* Running tests"
 echo "  + Test 1"
-test "$(./build/greeter --no-style --name Aitor)" = 'Kaixo Aitor!'
+if [ ! -x greeter ]; then
+    RUNNER_DIR="build/"
+else
+    RUNNER_DIR=""
+fi
+test "$(./$RUNNER_DIR/greeter --no-style --name Aitor)" = 'Kaixo Aitor!'
